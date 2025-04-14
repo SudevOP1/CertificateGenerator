8cd217508cf04da621766dbeb9d6509b387b5a82
SudevOP1 <sudevdahitule06@gmail.com>
2025-04-14 16:18:00 +0530

    qrcode done

--- qr_generator/generator.py
+++ qr_generator/generator.py
@@ -2,6 +2,17 @@ import qrcode
 
 qrcode_text      = "Yo! This is a QR🤯"
 output_file_name = "output.png"
+fill_color       = "#000000" # black
+back_color       = "#ffffff" # white
 
-img = qrcode.make(qrcode_text)
+qr_creater = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    box_size=10,
+    border=0,
+)
+qr_creater.add_data(qrcode_text)
+qr_creater.make(fit=True)
+img = qr_creater.make_image(fill_color=fill_color, back_color=back_color)
+
 img.save(output_file_name)
