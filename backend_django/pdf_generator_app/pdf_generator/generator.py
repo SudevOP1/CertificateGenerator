5aca9ef4af02e1edb66e8e9208cde4f7699c0acb
SudevOP1 <sudevdahitule06@gmail.com>
2025-04-15 22:50:19 +0530

    error in get_demo_certificate view

---
+++ backend_django/pdf_generator_app/pdf_generator/generator.py
@@ -0,0 +1,32 @@
+import jinja2, pdfkit, os, base64
+
+base_path = os.path.abspath(".").replace("\\", "/")
+template_file_name = "template.html"
+template_file_path = "./"
+pdfkit_exe_path = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
+
+config = pdfkit.configuration(wkhtmltopdf=pdfkit_exe_path)
+template_loader = jinja2.FileSystemLoader(template_file_path)
+template_env = jinja2.Environment(loader=template_loader)
+my_template = template_env.get_template(template_file_name)
+
+def get_pdf_file(organizer_name, workshop_name, date, qr, attendee_name, attendee_email):
+    qr_path     = f"file:///{base_path}/{qr}"
+    badge_path  = f"file:///{base_path}/badge.png"
+
+    context = {
+        "organizer_name":   organizer_name,
+        "workshop_name":    workshop_name,
+        "date":             date,
+        "qr_path":          qr_path,
+        "badge_path":       badge_path,
+        "attendee_name":    attendee_name,
+        "attendee_email":   attendee_email,
+    }
+
+    rendered_html = my_template.render(context)
+    # creating pdf in memory:
+    pdf_bytes = pdfkit.from_string(rendered_html, False, configuration=config, options={'enable-local-file-access': None})
+    # encoding pdf:
+    encoded_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
+    return encoded_pdf
--- backend_django/pdf_generator_app/views.py
+++ backend_django/pdf_generator_app/views.py
@@ -2,27 +2,67 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from PIL import Image
+from pdf_generator.generator import get_pdf_file
 
+def get_demo_qr(): return Image.open("./pdf_generator/demo_qr.png")
+
+def get_qr():       return "yo" # TODO
+
+@csrf_exempt
 def get_demo_certificate(request):
-    data = {
-        'message': 'This is a GET request',
-        'status': 'success'
+    """
+    POST body = {
+        "organizer_name": "Smit Doshi",
+        "workshop_name": "Python in 69 Hours",
+        "date": "dd-mm-yyyy",
+        "attendees": [
+            {"name": "Sahad Mithani", "email": "sahadmithani@gmail.com"},
+            {"name": "Sudev Dahitule", "email": "sudevdahitule@gmail.com"}
+        ]
     }
-    return JsonResponse(data)
+    """
+    if request.method == "POST":
+        try:
+            body_data = json.loads(request.body)
+            organizer_name = body_data["organizer_name"]
+            workshop_name = body_data["workshop_name"]
+            date = body_data["date"]
+            attendees = body_data["attendees"]
 
-def send_emails(request):
-    if request.method == 'POST':
+            attendee = attendees[0]
+            pdf_base64 = get_pdf_file(
+                organizer_name=organizer_name,
+                workshop_name=workshop_name,
+                date=date,
+                qr="demo_qr.png",
+                attendee_name=attendee["name"],
+                attendee_email=attendee["email"]
+            )
+
+            return JsonResponse({
+                "message": "success",
+                "certificate": pdf_base64,
+                "type": "base64-pdf"
+            }, status=200)
+
+        except Exception as e:
+            return JsonResponse({"message": "something went wrong", "error": str(e)}, status=400)
+    return JsonResponse({"message": "Only POST requests allowed"}, status=405)
+
+def send_emails(request): # TODO
+    if request.method == "POST":
         try:
             # Parse JSON data from the request body
             data = json.loads(request.body)
             # Process the data (just an example response)
             response_data = {
-                'received': data,
-                'message': 'POST request received successfully',
-                'status': 'success'
+                "received": data,
+                "message": "POST request received successfully",
+                "status": "success"
             }
             return JsonResponse(response_data, status=201)
         except json.JSONDecodeError:
-            return JsonResponse({'message': 'Invalid JSON'}, status=400)
+            return JsonResponse({"message": "Invalid JSON"}, status=400)
 
-    return JsonResponse({'message': 'Only POST requests are allowed'}, status=405)
+    return JsonResponse({"message": "Only POST requests are allowed"}, status=405)
